eb0795669803edf9e890b4f14bbd4533967c6b72
andoshin11 <shinglish11@gmail.com>
2020-04-04 06:22:03 +0900

    debug type error

--- src/createAST/index.ts
+++ src/createAST/index.ts
@@ -5,7 +5,7 @@ import { createRootAST } from './root'
 import { createModelType } from './models'
 import * as utils from '../utils'
 
-export const createAST = (data: OpenAPIObject) => {
+export const createAST = (data: number) => {
   // Generate Type alias for schemas
   const components = data.components
   const schemas = components && components.schemas
