9caec7a0665b23976a0ec8594c8843e58968bd65
andoshin11 <shinglish11@gmail.com>
2020-04-04 09:08:33 +0900

    Debug actions

--- src/createAST/index.ts
+++ src/createAST/index.ts
@@ -5,10 +5,10 @@ import { createRootAST } from './root'
 import { createModelType } from './models'
 import * as utils from '../utils'
 
-export const createAST = (data: number) => {
+export const createAST = (data: OpenAPIObject) => {
   // Generate Type alias for schemas
   const components = data.components
-  const schemas = components && components.schemas
+  const schemas = components && componentss.schemas
   const models = Object.entries(schemas || {}).map(([key, val]) => createModelType(key, val))
 
   const parsed = parse(data)
